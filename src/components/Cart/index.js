d3fc1b2592c32c6699a2432c89180cad89798ad3
Kancharla Mounika <kancharlamounika17@gmail.com>
2022-11-29 06:19:46 +0000

    authentication2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="c-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        className="c-image"
+        alt="cart"
+      />
+    </div>
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="p-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        className="p-image"
+        alt="products"
+      />
+    </div>
+  </>
+)
+
+export default Products
